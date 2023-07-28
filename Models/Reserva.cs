9a5790aa7d2cf8399153d899588ca5d37ddf2ecd
Natália Serrão <nataliaserrao45@gmail.com>
2023-07-28 11:09:09 -0400

    feature:implementacao sistema de hotel

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,11 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
+            if(hospedes.Count <= Suite.Capacidade){
                 Hospedes = hospedes;
             }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+            else{
+                throw new Exception("O número de hóspedes é superior à capacidade da suíte");
             }
         }
 
@@ -35,26 +30,21 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
             decimal valor = 0;
+            decimal desconto = 0;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                desconto = (DiasReservados * Suite.ValorDiaria) * Convert.ToDecimal(0.1);
+                valor = (DiasReservados * Suite.ValorDiaria) - desconto;
+                return valor;
             }
-
-            return valor;
+            return DiasReservados * Suite.ValorDiaria;
         }
     }
 }
